9896bc963855f7ec6f4437476060dfbdc2238c4d
android <android@LABFAE.local>
2024-10-09 22:36:58 -0300

    Erros ao criar um usuário

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,9 +2,10 @@ const jwt = require('jsonwebtoken')
 
 const auth = (req,res,next) => {
     //Array com as rotas que não precisam de segurança JWT, sempre coloque '/' seguido do nome da rota 
-    const nonSecurePath = ["/", "/login", "/register", "/forgetPassword", "/createUser"]
+    const nonSecurePath = ["login", "register", "forgetPassword", "createUser"]
     //Caso a rota estiver na array irá passar direto sem a verificação de JWT
-    if(nonSecurePath.includes(req.path)) return next()
+    let path = req.path.split("/")[1]
+    if(nonSecurePath.includes(path) || path == "") return next()
 
     try{
         jwt.verify(req.cookies.medcar_token, process.env.SECRET_KEY, null, 
@@ -17,12 +18,13 @@ const auth = (req,res,next) => {
         next()
     }catch(erro){
         let mensagem;
+        console.log(erro)
         if(erro.message == "jwt must be provided"){
             mensagem = "Parece que você não fez o login ainda"
         }else if(erro.message == "jwt has expired"){
             mensagem = "Sua sessão inspirou, faça o login novamente"
         }else{
-            mensagem = "Houve um erro, tente novamente mais tarde"
+            mensagem = "Houve um erro ao tentar fazer sua autenticação"
         }
         res.render("err/erro_mensagem", {erro_mensagem: mensagem})
     }
--- routes/usuario.js
+++ routes/usuario.js
@@ -9,6 +9,15 @@ usuarioRouter.post("/createUser", (req,res)=>{
     let cpf = req.body.cpf ?? null
     let celular = req.body.celular ?? null
 
+    console.log(email)
+
+    usuarioModel.findOne({where: {email: email}})
+        .then((user) => {
+            if(user != null){
+                res.redirect("/register/Email já cadastrado")
+            }
+        })
+
     usuarioModel.create({
         email: email,
         senha: senha,
@@ -27,13 +36,6 @@ usuarioRouter.post("/createUser", (req,res)=>{
             httpOnly: true,
         })
         res.redirect("/")
-    }).catch((erro) => {
-        if(erro.errors[0].type == "unique violation"){
-            res.redirect("/register/Email já cadastrado")
-        }else{
-            res.redirect("/register/Não foi possível fazer o cadastro")
-        }
-        
     })
 })
 
