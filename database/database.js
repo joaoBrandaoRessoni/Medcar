814ab841c463b694c0e1dcf236db7641dbdf276d
Igru808 <igormarianopirola@gmail.com>
2024-12-19 09:48:03 -0300

    update

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const sequelize = require("sequelize")
 
-const connection = new sequelize("medcar", "root", "1234", {
-    host:"localhost",
+const connection = new sequelize("medcar", "root", "", {
+    host:"186.219.147.57",
     dialect:"mysql",
     port: 3306,
     timezone: "-03:00"
